8c8b6e9a88e9bf4c90ef58e2560b0f5d314c42b5
simperialtd <101255999+simperialtd@users.noreply.github.com>
2024-10-23 11:01:53 +0000

    Fix issue with Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ if(builder.Environment.IsDevelopment())
  {
      builder.Services.AddDbContext<MyDatabaseContext>(options =>
          options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-     builder.Services.AddStackExchangeRedisCache(options =>
+     //builder.Services.AddStackExchangeRedisCache(options =>
      //{
      //options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
      //options.InstanceName = "SampleInstance";
